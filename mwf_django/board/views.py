a482789e5d9eb5ca0cb8a20d70a1719a89091114
cole <colepolyak@gmail.com>
2020-10-06 11:21:59 -0600

    Fixed post ordering

--- mwf_django/board/views.py
+++ mwf_django/board/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from .models import BulletinPost
 from .serializers import BulletinPostSerializer
 from rest_framework import generics
+import datetime
+from pytz import timezone
 
+def fix_date(obj):
+    obj.created_at = obj.created_at.replace(tzinfo=timezone('US/Mountain')).strftime("%B %d, %Y at %H:%M %p")
+    return obj
 
 # Create your views here.
 class BulletinPostCreate(generics.ListCreateAPIView):
-		queryset = BulletinPost.objects.all()
-		serializer_class = BulletinPostSerializer 
+    queryset = BulletinPost.objects.all().order_by('-created_at')
+    queryset = map(fix_date, queryset)
+
+    serializer_class = BulletinPostSerializer
