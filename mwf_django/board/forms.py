fde5a333b2487b2d84eae185efe7ced4386a75b5
cole <colepolyak@gmail.com>
2020-10-07 09:10:23 -0600

    Bits a pieces

--- mwf_django/board/forms.py
+++ mwf_django/board/forms.py
@@ -1,7 +1,37 @@
 from django import forms
 from board.models import BulletinPost
+from django.contrib.auth.models import User
+from board.models import UserProfile
 
 class BulletinPostForm(forms.ModelForm):
 	class Meta():
 		model = BulletinPost
 		fields = ('user', 'message')
+
+class UserForm(forms.ModelForm):
+	password = forms.CharField(widget=forms.PasswordInput())
+	repeat_password = forms.CharField(widget=forms.PasswordInput())
+
+	class Meta():
+		model = User
+		fields = ('username', 'email', 'password')
+
+	def clean_new_password2(self):
+            password1 = self.cleaned_data.get('password')
+            password2 = self.cleaned_data.get('repeat_password')
+            if password1 and password2:
+                if password1 != password2:
+                    raise forms.ValidationError(
+                        self.error_messages['password_mismatch'],
+                        code='password_mismatch',
+                    )
+              # I think this line is checking whether the user's password matches their
+              # previous password.
+#             password_validation.validate_password(password2, self.user)
+            return password2
+
+
+class UserProfileForm(forms.ModelForm):
+	class Meta():
+		model = UserProfile
+		fields = ('portfolio_site', 'profile_pic')
--- mwf_django/board/models.py
+++ mwf_django/board/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+
+	# Our foreign key to an existing user.
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+	# User doesn't have to fill it out.
+	portfolio_site = models.URLField(blank=True)
+	profile_pic = models.ImageField(upload_to='profile-pictures', blank=True)
+
+	def __str__(self):
+		return self.user.username
 
 
 class BulletinPost(models.Model):
